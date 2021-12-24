6dce28b0c7283aa45e1bab6648ea6d04cb20c81d
wagnerkaba <bom.link+github@gmail.com>
2021-12-24 10:27:41 -0300

    implementação do package REGISTRATION

--- src/main/java/com/wagner/loginBackend/appuser/AppUserService.java
+++ src/main/java/com/wagner/loginBackend/appuser/AppUserService.java
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
-    private final static String USER_NOT_FOUND_MSG = "USER WITH EMAIL %s NOT FOUND";
+    private final static String USER_NOT_FOUND = "USER WITH EMAIL %s NOT FOUND";
     private final AppUserRepository appUserRepository;
 
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
-                .orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+                .orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
     }
 }
---
+++ src/main/java/com/wagner/loginBackend/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.wagner.loginBackend.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "WORKS";
+    }
+}
