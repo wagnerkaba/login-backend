419c2b370fbf1fcabab44ef1f34f6c45e81273c9
wagnerkaba <bom.link+github@gmail.com>
2021-12-24 09:05:35 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# API para login e registro de usuário + verificação de email 
+
+
+
+
+
+## Tecnologias utilizadas
+
+* Java 11
+* Lombok
+* Spring Web
+* Spring Security
+* Spring Data JPA
+* PostgreSQL
+* Java Mail Sender
---
+++ src/test/java/com/wagner/loginBackend/LoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.loginBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
